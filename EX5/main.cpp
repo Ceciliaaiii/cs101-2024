595ab9644f309362fc4184eb19c032fc5cd0e440
B1229030 <144075736+Ceciliaaiii@users.noreply.github.com>
2024-05-23 14:01:33 +0800

    Update main.cpp

--- EX5/main.cpp
+++ EX5/main.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+class myStyring{
+    private:
+        string m_str;
+    public:
+        myString(string s){
+            m_str = s;
+        }
+}
+class Readclass{
+    
+}
+
+int main()
+{
+   Readclass refile;
+   refile.showClass();
+   return 0;
+}
